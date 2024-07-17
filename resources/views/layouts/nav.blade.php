99822d352622531f0230a48ec037fe3fc71a0787
Shivam Sham Londhe <shivamlondhe91@gmail.com>
2024-07-17 16:41:03 +0530

    this is first commit. creating new project and manneging files and data table

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="{{route("home")}}">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{route("home")}}">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="{{route("home")}}" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="{{route("home")}}">Action</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="d-flex">
+        <a href="{{route("login")}}" class="btn btn-outline-success mx-1">Login</a>
+        <a href="{{route("user.create")}}" class="btn btn-outline-success mx-1">Register</a>
+      </div>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,24 @@
 <?php
 
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name("home");
+Route::get('login', function () {
+    return view('login');
+})->name("login");
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::resource("user", User::class);
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
